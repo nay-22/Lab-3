acf089f9376ef0d0e6571b96b9f910c5bbc108b2
Nayan <nayan92001@gmail.com>
2023-04-23 10:24:49 +0530

    java addition initial commit

---
+++ AddTwoNum.java
@@ -0,0 +1,9 @@
+class AddTwoNum {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        int sum = a + b;
+        System.out.println("int a = " + a);
+        System.out.println("int b = " + b);
+        System.out.println("int sum = " + sum);
+    }
+}
